0973372dc5a75c20bd4c0f9f8f9b47aba2f670ae
KEATON-RYZEN\rapie <rapier83@gmail.com>
2020-10-23 00:03:59 +0900

    trdata_slot에서 오타가 났음 tr_data_slot -> trdata_slot 으로 정정

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -12,13 +12,19 @@ class Kiwoom(QAxWidget):
         # Collection of variables to run event_loop
         self.login_event_loop = QEventLoop()
 
-        # Activate initial setting functions
-        #self.kw = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        # Variables of account
+        self.account_num = None
+        self.deposit = 0                                # 예수금
+        self.use_money = 0                              # 실제 투자에 사용될 금액
+        self.use_money_rate = 0.5                       # 에수금에서 실제 사용할 금액의 비율
+        self.output_deposit = 0                         # 출력가능금액
 
+        # Activate initial setting functions
         self.get_ocx_instance()
         self.event_slots()
         self.signal_login_commConnect()
         self.get_account_info()
+        self.detail_account_info()
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
@@ -29,6 +35,7 @@ class Kiwoom(QAxWidget):
 
     def event_slots(self):
         self.OnEventConnect.connect(self.login_slot)
+        self.OnReceiveTrData.connect(self.trdata_slot)  # 트랜젝션 요청관련
 
     def login_slot(self, err_code):
         print(errors(err_code)[1])
@@ -41,3 +48,27 @@ class Kiwoom(QAxWidget):
         self.account_num = account_num
 
         print('Account Number: %s' % account_num)
+
+    def detail_account_info(self, sPrevNext="0"):
+        self.dynamicCall("SetInputValue(QString, QString)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(QString, QString)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(QString, QString)", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(QString, QString)", "조회구분", "1")
+        self.dynamicCall("SetInputValue(QString, QString, int, QString)",
+                         "예수금상세현황요청", "opw00001", sPrevNext, self.screen_my_info)
+
+    def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
+        if sRQName == "예수금상세현황요청":
+            deposit = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, "예수금")
+            self.deposit = int(deposit)
+
+            use_money = float(self.deposit) * self.use_money_rate
+            self.use_money = int(use_money)
+            self.use_money = self.use_money / 4                             # 한 종목에 돈을 다쓰지 않게 4종목에 나눠서
+
+            output_deposit = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, "출금가능금액")
+            self.output_deposit = int(output_deposit)
+
+            print("예수금: $s" % self.output_deposit)
+
+            self.stop_screen_cancel(self.screen_my_info)
